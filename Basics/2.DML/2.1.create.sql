8f0d3b4c88da7de3319f0fa73378b2270e01fa8c
sbkr74 <sbkr74@gmail.com>
2024-04-09 23:40:33 +0530

    2.1.create.sql file added

---
+++ Basics/2.DML/2.1.create.sql
@@ -0,0 +1,12 @@
+--HOW TO CREATE TABLE
+
+create schema sample;
+create table sample.Employee(
+EmpID int,
+EmpName varchar(20),
+DeptID int,
+Salary int,
+Bonus int,
+Gender varchar(30),
+City varchar(30),
+DOJ date);
