73880e77b23d912a1403256e8557a026fa25953a
sbkr74 <sbkr74@gmail.com>
2024-04-09 23:44:52 +0530

    1.1.select.sql file updated2.0

--- Basics/1.DQL/1.1.select.sql
+++ Basics/1.DQL/1.1.select.sql
@@ -1,3 +1,10 @@
 --Syntax
 -- select '*/column_name/agg_fun/' from 'table_name';
- select * from emp;
+
+-- using * to view whole table with all the columns
+ select * from Employee;
+
+ -- using column_name to view table with particular column 
+ select EmpID from Employee;
+ -- multiple column_name
+ select EmpName,DOJ,Salary from Employee;
