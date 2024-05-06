7d9d0d3d6045661a0e96f7c67daae906537dc252
sbkr74 <sbkr74@gmail.com>
2024-05-06 22:40:34 +0530

    2.2.alter.sql file updated2.1(modified comments)

--- Basics/2.DDL/2.2.alter.sql
+++ Basics/2.DDL/2.2.alter.sql
@@ -1,8 +1,8 @@
--- add column
+-- 1.add column
 alter table Employee
 add phonenumber varchar(20);
 
--- drop column
+-- 2. drop column
 alter table Employee
 drop phonenumber varchar(20);
 
@@ -10,24 +10,24 @@ drop phonenumber varchar(20);
 ALTER TABLE table_name
 RENAME COLUMN old_name to new_name; */
 
--- rename column
+--3.  rename column
 alter table Employee
 rename phonenumber to mobilenumber;
 
 --syntax for renaming a column is different in SQL server.
 EXEC sp_rename 'table_name.old_name',  'new_name', 'COLUMN'; 
 
--- modify datatype
+--4.  modify datatype
 
---1. SQL Server / MS Access:
+--4.1. SQL Server / MS Access:
 ALTER TABLE table_name
 ALTER COLUMN column_name datatype; 
 
--- 2. My SQL / Oracle (prior version 10G):
+--4.2. My SQL / Oracle (prior version 10G):
 ALTER TABLE table_name
 MODIFY COLUMN column_name datatype; 
 
--- 3. Oracle 10G and later:
+--4.3. Oracle 10G and later:
 ALTER TABLE table_name
 MODIFY column_name datatype; 
 
