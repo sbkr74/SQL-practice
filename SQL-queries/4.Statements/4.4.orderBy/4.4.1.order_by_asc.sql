0e4c5b583f457b938730ea9189033ebe0116bfc3
sbkr74 <sbkr74@gmail.com>
2024-05-09 23:29:30 +0530

    4.4.1.order_by_asc.sql file added

---
+++ SQL-queries/4.Statements/4.4.orderBy/4.4.1.order_by_asc.sql
@@ -0,0 +1,5 @@
+-- using order by clause 
+-- In order by for ascending order their is no need of using asc keyword.
+select * from practical.Emp order by Salary;
+-- using asc keyword
+select * from practical.Emp order by Salary asc;
