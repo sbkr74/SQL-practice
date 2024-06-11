5f2dba138f607cef77e90776cc525452979f8d11
sbkr74 <sbkr74@gmail.com>
2024-06-11 22:14:28 +0530

    Q10.sql file added

---
+++ 30Questions/Q10.sql
@@ -0,0 +1,2 @@
+-- Write an SQL query to print the FIRST_NAME and LAST_NAME from the Worker table into a single column COMPLETE_NAME. A space char should separate them.
+SELECT CONCAT(FIRST_NAME,' ',LAST_NAME) AS COMPLETE_NAME FROM WORKER
