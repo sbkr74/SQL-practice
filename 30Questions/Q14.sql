5266f36c46dc999da8ee33d86904cdb8f0a7951f
sbkr74 <sbkr74@gmail.com>
2024-07-06 23:16:40 +0530

    Q14.sql file updated2.0

--- 30Questions/Q14.sql
+++ 30Questions/Q14.sql
@@ -1,2 +1,4 @@
 -- Write an SQL query to print details of workers excluding first names, "Vipul" and "Satish" from the Worker table.
 SELECT * FROM WORKER WHERE FIRST_NAME NOT IN ("Vipul","Satish");
+-- Another Query
+SELECT * FROM WORKER WHERE FIREST_NAME NOT LIKE 'Vipul%' AND NOT LIKE 'Satish%'
