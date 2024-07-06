bf5a9385d0e9f1e25bbd4292c966b317a3ee1189
sbkr74 <sbkr74@gmail.com>
2024-07-06 23:05:57 +0530

    Q13.sql file updated2.0

--- 30Questions/Q13.sql
+++ 30Questions/Q13.sql
@@ -1,2 +1,4 @@
--- Write an SQL query to print details for Workers with the first names “Vipul” and “Satish” from the Worker table.
+-- Write an SQL query to print details for Workers with the first names 'Vipul' and 'Satish' from the Worker table.
 SELECT * FROM WORKER WHERE FIRST_NAME = 'Vipul' AND FIRST_NAME = 'Satish';
+-- another query
+SELECT * FROM WORKER WHERE FIREST_NAME IN ('Vipul','Satish');
