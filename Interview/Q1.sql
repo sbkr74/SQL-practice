bdd95a030e5b7a967bb4c4c0075bb2010c4742b6
sbkr74 <sbkr74@gmail.com>
2024-08-24 10:11:00 +0530

    used rank()

--- Interview/Q1.sql
+++ Interview/Q1.sql
@@ -1,8 +1,11 @@
 -- write sql query to find 2 maximum salary
 -- col: emp_id,emp_name,salary,dept
 
---using subquery
+-- using subquery
 SELECT MAX(E1.salary) FROM employee e1 WHERE  E1.salary<(SELECT MAX(E2.salary) FROM employee E2)
 
---CO-RELATED SUBQUERY
+-- CO-RELATED SUBQUERY
 SELECT W1.Salary FROM practical.worker W1 WHERE 2 = (SELECT COUNT(DISTINCT(W2.Salary)) FROM practical.worker W2 WHERE W1.Salary<=W2.Salary)
+
+-- using Windows function
+SELECT Salary FROM (SELECT Salary,RANK() OVER (ORDER BY Salary DESC) AS max_sal FROM practical.worker) AS RNK_SAL WHERE max_sal=2;
