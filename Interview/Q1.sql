ff04b0bd58c23ec1e02a59bededdd41c11b48b39
sbkr74 <sbkr74@gmail.com>
2024-08-24 10:03:16 +0530

    co-related subquery

--- Interview/Q1.sql
+++ Interview/Q1.sql
@@ -4,3 +4,5 @@
 --using subquery
 SELECT MAX(E1.salary) FROM employee e1 WHERE  E1.salary<(SELECT MAX(E2.salary) FROM employee E2)
 
+--CO-RELATED SUBQUERY
+SELECT W1.Salary FROM practical.worker W1 WHERE 2 = (SELECT COUNT(DISTINCT(W2.Salary)) FROM practical.worker W2 WHERE W1.Salary<=W2.Salary)
