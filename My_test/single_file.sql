eb9d25fdb0d57b47193545a98f7303d592c09a63
sbkr74 <sbkr74@gmail.com>
2024-10-09 21:19:36 +0530

    quest_2 added

--- My_test/single_file.sql
+++ My_test/single_file.sql
@@ -6,3 +6,11 @@ Write a SQL query to find the total sales for each product from a `sales` table.
 SELECT product_id,SUM(sale_amount) AS total_sale FROM sales
 GROUP BY product_id;
 
+/*
+Question 2:
+Write a SQL query to find the second highest sale_amount from the sales table.
+*/
+
+SELECT MAX(sale_amount) AS second_highest
+FROM sales
+where sale_amount< (SELECT MAX(sale_amount) FROM sales);
