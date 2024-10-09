6cad6b8df3ecd0d273b854577fc0da08e0f4c952
sbkr74 <sbkr74@gmail.com>
2024-10-09 21:29:43 +0530

    quest_3 added

--- My_test/single_file.sql
+++ My_test/single_file.sql
@@ -1,6 +1,7 @@
 /*
 **Question 1:**  
-Write a SQL query to find the total sales for each product from a `sales` table. The table has the following columns: `product_id`, `sale_amount`, and `sale_date`.
+Write a SQL query to find the total sales for each product from a `sales` table. 
+The table has the following columns: `product_id`, `sale_amount`, and `sale_date`.
 */
 
 SELECT product_id,SUM(sale_amount) AS total_sale FROM sales
@@ -22,3 +23,13 @@ FROM sales
 ORDER BY sale_amount DESC
 LIMIT 1 OFFSET 1;
 
+/*
+Question 3:
+Write a SQL query to find all products that have never been sold (i.e., products that do not appear in the sales table). 
+and sales` table having columns: `product_id`, `sale_amount`, and `sale_date`
+Assume there is a products table with columns product_id and product_name.
+*/
+
+SELECT p.product_id,p.product_name
+FROM product p
+WHERE product_id NOT IN (SELECT DISTINCT product_id FROM sales);
