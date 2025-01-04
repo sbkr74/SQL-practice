0cdfdb8b127b25aab33272f45c5f404c50946094
sbkr74 <sbkr74@gmail.com>
2025-01-04 22:17:25 +0530

    correction in solution

--- 150SQL/1.normal_query.sql
+++ 150SQL/1.normal_query.sql
@@ -5,18 +5,22 @@ SELECT EMPNO,ENAME,JOB,MGR,HIREDATE,SAL,COMM,DEPTNO FROM EMP;
 -- 2. Display unique Jobs from EMP table?
 SELECT DISTINCT(JOB) FROM EMP;
 -- 3. List the emps in the asc order of their Salaries?
-SELECT ENAME,SAL FROM EMP ORDER BY SAL;
+SELECT * FROM EMP ORDER BY SAL ASC;
 -- 4. List the details of the emps in asc order of the Dptnos and desc of Jobs?
-SELECT * FROM EMP ORDER BY DEPTNO, JOBS DESC;
+SELECT * FROM EMP ORDER BY DEPTNO ASC, JOB DESC;
 -- 5. Display all the unique job groups in the descending order?
-SELECT DISTINCT(JOBS) FROM EMP ORDER BY JOBS DESC;
+SELECT DISTINCT(JOB) FROM EMP ORDER BY JOB DESC;
 -- 6. Display all the details of all ‘Mgrs’
 SELECT * FROM EMP WHERE JOB = 'MANAGER';
+/* Subquery */
+SELECT * FROM EMP WHERE EMPNO IN (SELECT MGR FROM EMP);
 -- 7. List the emps who joined before 1981.
-SELECT ENAME FROM EMP WHERE YEAR(HIREDATE) < 1981;
+SELECT * FROM EMP WHERE HIREDATE < ('1981-01-01');
+/*Using DATE functions*/
+SELECT * FROM EMP WHERE YEAR(HIREDATE) < 1981;
 -- 8. List the Empno, Ename, Sal, Daily sal of all emps in the asc order of Annsal.
-SELECT EMPNO,ENAME,SAL,(SAL/30) AS DailySal FROM EMP ORDER BY (SAL*12);
+SELECT EMPNO,ENAME,SAL,(SAL/30) AS DailySal,(SAL*12) AS Annsal FROM EMP ORDER BY Annsal;
 -- 9. Display the Empno, Ename, job, Hiredate, Exp of all Mgrs
-SELECT EMPNO, ENAME, JOB, HIREDATE, YEAR(CURRENT_DATE())-YEAR(HIREDATE) AS EXP FROM EMP WHERE JOB = 'MANAGER';
+SELECT EMPNO, ENAME, JOB, HIREDATE, YEAR(CURRENT_DATE())-YEAR(HIREDATE) AS EXP FROM EMP WHERE EMPNO IN (SELECT MGR FROM EMP);
 -- 10. List the Empno, Ename, Sal, Exp of all emps working for Mgr 7369.
 SELECT EMPNO,ENAME,SAL,YEAR(CURRENT_DATE())-YEAR(HIREDATE) AS EXP FROM EMP WHERE MGR = 7369;
