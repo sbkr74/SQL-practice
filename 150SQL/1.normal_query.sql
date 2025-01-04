0608a6d1ba88d70b55145dafe89558af5f8dbbb2
sbkr74 <sbkr74@gmail.com>
2025-01-04 20:54:11 +0530

    1.normal_queries.sql file added with questions

---
+++ 150SQL/1.normal_query.sql
@@ -0,0 +1,19 @@
+-- 1. Display all the information of the EMP table?
+
+-- 2. Display unique Jobs from EMP table?
+
+-- 3. List the emps in the asc order of their Salaries?
+
+-- 4. List the details of the emps in asc order of the Dptnos and desc of Jobs?
+
+-- 5. Display all the unique job groups in the descending order?
+
+-- 6. Display all the details of all ‘Mgrs’
+
+-- 7. List the emps who joined before 1981.
+
+-- 8. List the Empno, Ename, Sal, Daily sal of all emps in the asc order of Annsal.
+
+-- 9. Display the Empno, Ename, job, Hiredate, Exp of all Mgrs
+
+-- 10. List the Empno, Ename, Sal, Exp of all emps working for Mgr 7369.
