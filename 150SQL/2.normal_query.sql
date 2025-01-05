6a69c00a972b60aaae23c9a3f57e0fec3f054188
sbkr74 <sbkr74@gmail.com>
2025-01-05 11:02:08 +0530

    2.normal_query.sql questions added

---
+++ 150SQL/2.normal_query.sql
@@ -0,0 +1,19 @@
+-- 21. List the Enames those are starting with 'S' and with five characters.
+
+-- 22. List the emps those are having four chars and third character must be 'r'.
+
+-- 23. List the Five character names starting with 'S' and ending with 'H'.
+
+-- 24. List the emps who joined in January.
+
+-- 25. List the emps who joined in the month of which second character is ‘a’.
+
+-- 26. List the emps whose Sal is four digit number ending with Zero.
+
+-- 27. List the emps whose names having a character set 'll' together.
+
+-- 28. List the emps those who joined in 80's.
+
+-- 29. List the emps who does not belong to Deptno 20.
+
+-- 30. List all the emps except 'PRESIDENT' & 'MGR' in asc order of Salaries.
