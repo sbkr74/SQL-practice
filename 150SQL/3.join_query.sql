ac710e9d0dea014fb4a2c9f446fcc7f665563bd5
sbkr74 <sbkr74@gmail.com>
2025-01-09 19:37:15 +0530

    3.join_query.sql file added with question set

---
+++ 150SQL/3.join_query.sql
@@ -0,0 +1,30 @@
+/* 41. Display the Empno, Ename, Sal, Dname, Loc, Deptno, Job of all emps
+working at CJICAGO or working for ACCOUNTING dept with Ann
+Sal>28000, but the Sal should not be=3000 or 2800 who doesn’t belongs to the
+Mgr and whose no is having a digit ‘7’ or ‘8’ in 3rd position in the asc order of
+Deptno and desc order of job.
+*/
+
+/* 42. Display the total information of the emps along with Grades in the asc order. */
+
+/* 43. List all the Grade2 and Grade 3 emps. */
+
+/* 44. Display all Grade 4,5 Analyst and Mgr. */
+
+/*45. List the Empno, Ename, Sal, Dname, Grade, Exp, and Ann Sal of emps working for 
+Dept10 or20. */
+
+/* 46. List all the information of emp with Loc and the Grade of all the emps
+belong to the Grade range from 2 to 4 working at the Dept those are not starting
+with char set ‘OP’ and not ending with ‘S’ with the designation having a char ‘a’
+any where joined in the year 1981 but not in the month of Mar or Sep and Sal
+not end with ‘00’ in the asc order of Grades.
+*/
+
+/* 47. List the details of the Depts along with Empno, Ename or without the emps. */
+
+/* 48. List the details of the emps whose Salaries more than the employee BLAKE. */
+
+/* 49. List the emps whose Jobs are same as ALLEN. */
+
+/* 50. List the emps who are senior to King. */
