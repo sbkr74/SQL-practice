865b25eb4b84652cd299333fe7ff2f516631c111
sbkr74 <sbkr74@gmail.com>
2025-01-10 14:33:09 +0530

    4.subquery.sql file added

---
+++ 150SQL/4.subquery.sql
@@ -0,0 +1,26 @@
+-- 51. List the Emps who are senior to their own MGRS.
+
+-- 52. List the Emps of Deptno 20 whose Jobs are same as Deptno10.
+
+-- 53. List the Emps whose Sal is same as FORD or SMITH in desc order of Sal.
+
+-- 54. List the emps Whose Jobs are same as MILLER or Sal is more than ALLEN.
+
+-- 55. List the Emps whose Sal is > the total remuneration of the SALESMAN.
+
+-- 56. List the emps who are senior to BLAKE working at CHICAGO & BOSTON.
+
+/* 57. List the Emps of Grade 3,4 belongs to the dept ACCOUNTING and
+RESEARCH whose Sal is more than ALLEN and exp more than SMITH in the
+asc order of EXP. */
+
+-- 58. List the emps whose jobs same as SMITH or ALLEN.
+
+/* 59. Write a Query to display the details of emps whose Sal is same as of
+a) Employee Sal of EMP1 table.
+b) ¾ Sal of any Mgr of EMP2 table.
+c) The sal of any person with exp of 5 years belongs to the sales dept of emp3 table.
+d) Any grade 2 employee of emp4 table.
+e) Any grade 2 and 3 employee working fro sales dept or operations dept joined in 89. */
+
+-- 60. Any jobs of deptno 10 those that are not found in deptno 20.
